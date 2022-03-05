04f755db4d671524dbff508e0b3b167d6c46a9ed
Angelique Abacajan <ma_utsey@yahoo.com>
2022-03-05 13:33:44 -0500

    Implemented a program to take an array of arrays and flatten to a single array

--- session2.js
+++ session2.js
@@ -84,7 +84,26 @@ function chunkArray(arr, len) {
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
   
-function flattenArray(arrays) {}
+function flattenArray(arrays) {
+    // SOLUTION 1
+    // Reduce
+    // return arrays.reduce(function(a, b) {
+    //     return a.concat(b);
+    // });
+
+    // SOLUTION 2
+    // return [].concat.apply([], arrays);
+
+    // // SOLUTION 3
+    // function add(a, b, c) {
+    //     return a + b + c;
+    // }
+
+    // const arr = [1, 2, 3];
+    // console.log(add(...arr));
+
+    // return [].concat(...arrays);
+}
   
 // CHALLENGE 4: ANAGRAM
 // Return true if anagram and false if not
@@ -101,7 +120,7 @@ function isAnagram(str1, str2) {}
 function letterChanges(str) {}
   
 // Call Function
-const output = chunkArray([1, 2, 3, 4, 5, 6, 7], 2);
+const output = flattenArray([1, 2], [3, 4], [5, 6], [7]);
   
  console.log(output);
   
