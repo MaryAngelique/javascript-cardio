1ef543c240d6a207fc830c52ec965ed9b1c77db8
Angelique Abacajan <ma_utsey@yahoo.com>
2022-03-04 23:11:51 -0500

    Implemented a program that prints all the numbers from 1 to 100

--- session1.js
+++ session1.js
@@ -123,7 +123,7 @@ function maxCharacter(str) {
     let maxChar = "";
 
     str.split("").forEach(function(char) {
-        
+
         if (chars[char]) {
             chars[char]++;
 
@@ -149,11 +149,26 @@ function maxCharacter(str) {
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+
+        } else if (i % 5 === 0){
+            console.log("Buzz");
+
+        } else {
+            console.log(i);
+        }
+    }
+}
 
 
 
 // Call Function
-const output = maxCharacter('javascript');
+const output = fizzBuzz('javascript');
 
 console.log(output);
